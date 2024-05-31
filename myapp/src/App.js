ca9e5c527eaf18ca766439c0719ecd4d29b4e737
Avnish Patel <avnishpatel855@gmail.com>
2024-05-31 13:56:47 +0530

    For live cheking

---
+++ myapp/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is the our first React Project 
+        </p>
+        <p>
+          This The also Pushed on our GitHub Rapo
+        </p>
+        <p> Added para for live check local to dynamic url changes</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
